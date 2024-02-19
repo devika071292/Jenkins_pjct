96f3d78793e56d38a7a71b80a97a51368b53408f
Devika <Devika@DESKTOP-018S02M>
2024-02-19 13:52:30 +0530

    set eg

---
+++ Jenkins_Projct/src/main/java/setcollectionpckg/SetExample1.java
@@ -0,0 +1,19 @@
+package setcollectionpckg;
+
+import java.util.*;    
+
+public class SetExample1 {
+
+	  public static void main(String[] args)   
+	    {   
+	        // creating LinkedHashSet using the Set  
+	        Set<String> data = new LinkedHashSet<String>();   
+	    
+	        data.add("JavaTpoint");   
+	        data.add("Set");   
+	        data.add("Example");   
+	        data.add("Set");   
+	    
+	        System.out.println(data);   
+	    }   
+}
